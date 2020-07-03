3b1c676d504e791228352d5ed22205d4ef032cb0
Stephan Zeissler <stephan@stormforger.com>
2020-07-03 11:33:02 +0200

    added testapp and gorillatoolkit mutex

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/stormforger/testapp/server"
+)
 
 func main() {
+	r := mux.NewRouter()
+	server.RegisterStaticHandler(r)
+
 	fmt.Println("Hello World!")
+	http.ListenAndServe(":8080", r)
 }
