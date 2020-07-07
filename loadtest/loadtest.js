3b57bc677df8ae74bc96fbf688349bc554aa0f6e
Stephan <stephan@stormforger.com>
2020-07-07 14:15:07 +0200

    Add NFR example usage (#6)
    * add some more meat to the test-case
    
    * Use NFR file for production test-run
    
    * use underscores for tags
    
    * fix validation errors
    
    * more nfr grooming

--- loadtest/loadtest.js
+++ loadtest/loadtest.js
@@ -16,11 +16,63 @@ definition.session("country-lookup", function(session) {
   var countries = session.ds.loadStructured($dsPrefix + "countries-de.csv");
   var country = session.ds.pickFrom(countries);
 
-  session.get("/?country=:country", {
-    tag: "get-country",
+  visitLandingPage(session, country);
+  login(session);
+  viewProduct(session, country);
+});
+
+
+function visitLandingPage(session, country) {
+  session.get("/:country/", {
+    tag: "t100_landing",
     params: {
       country: country.get("code"),
     },
   });
-  session.check("mycheck", session.lastHttpStatus(), "<=", 400);
-});
+  session.check("c100_landing", session.lastHttpStatus(), "<=", 400);
+}
+
+function login(session) {
+  // in this example we just fetch a session token and use that for the rest of the requests
+  session.post("/random/get_token", {
+    tag: "t200_login",
+    extraction: {
+      jsonpath: {
+        "auth_token": "$.token",
+      }
+    },
+  });
+  session.assert("c200_login", session.getVar("auth_token"), "!=", "");
+
+  // Our only shop accepts the auth token in the Authorization header.
+  session.defaults.setAuthentication({
+    username: "",
+    password: session.getVar("auth_token"),
+  });
+}
+
+function viewProduct(session, country) {
+  session.get("/data/test.json?country=:country", {
+    tag: "t300_viewproduct",
+    params: {
+      country: country.get("code"),
+    },
+    abort_on_error: true,
+    extraction: {
+      jsonpath: {
+        product: "$.root.list[0].id",
+      }
+    }
+  });
+  session.if(session.getVar("product"), "!=", "", function(ctx) {
+    // productID is not empty
+    ctx.get("/?country=:country&product=:product", {
+      tag: "t310_viewproduct",
+      abort_on_error: true,
+      params: {
+        country: country.get("code"),
+        product: session.getVar("product"),
+      }
+    });
+  });
+}
